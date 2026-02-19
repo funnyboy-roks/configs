856351a78b9ffc3583ba2ba4d16ef6ef4cf4de7d
funnyboy-roks <fbr@fbr.dev>
2026-02-19 12:50:50 -0600

    i3: discord time prompt zsh->sh

--- i3/discord-time-prompt.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-#
-# Prompts (using dmenu) for a time and then copies the discord format
-# to the keyboard and prints to stdout
-
-source ~/.zshrc
-
-dt $(echo "$1" "$(dmenu -fn 'Anonymous Pro-24' < /dev/null)" | cut -d' ' -f1-)
